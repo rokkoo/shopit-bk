064eac18a4bc7c7cdbd5cae3a9769384d72d7c7f
alfonso <alfonsolasaguirre@gmail.com>
2023-07-26 17:19:53 +0200

    add lists endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import { ShopListController } from "./shopListController";
+import ShopListIconController from "./shopListController";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server started on PORT:${port}`);
 });
 
-app.get("/test", ShopListController);
+app.get("/icons", ShopListIconController.getIcons);
+app.post("/createList", ShopListIconController.createList);
+app.get("/getList", ShopListIconController.getLists);
 
 export default app;
--- src/shopListController/index.ts
+++ src/shopListController/index.ts
@@ -2,11 +2,47 @@ import { Request, Response } from "express";
 import { Deta } from "deta";
 
 const deta = Deta();
-const files = deta.Drive("icons");
+const db = deta.Base("shopit_icons_db");
+const LIST_DB = deta.Base("shopit_list_db");
 
-export const ShopListController = async (req: Request, res: Response) => {
-  const list = await files.get("hello.txt");
+export const getIcons = async (req: Request, res: Response) => {
+  const list = await db.fetch();
   console.log(list);
 
-  res.status(200).json({ hola: "it works2", list });
+  res.status(200).json({
+    list,
+  });
 };
+
+const createList = async (req: Request, res: Response) => {
+  const { title } = req.body;
+
+  if (!title) {
+    res.status(402).json({ error: "missing title field" });
+    return;
+  }
+
+  const list = await LIST_DB.put({
+    title,
+  });
+
+  res.status(200).json({
+    list,
+  });
+};
+
+const getLists = async (req: Request, res: Response) => {
+  const lists = await LIST_DB.fetch();
+
+  res.status(200).json({
+    lists,
+  });
+};
+
+const ShopListIconController = {
+  getIcons,
+  createList,
+  getLists,
+};
+
+export default ShopListIconController;
